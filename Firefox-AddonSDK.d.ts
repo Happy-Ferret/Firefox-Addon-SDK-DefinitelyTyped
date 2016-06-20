4dac855f3d24b268bb0944b4b38a1af9cab1a9b1
Mohammed Hamdy <mohammed.hamdy777@gmail.com>
2016-06-20 10:26:13 +0200

    Add: typed base64 module

--- Firefox-AddonSDK.d.ts
+++ Firefox-AddonSDK.d.ts
@@ -3,3 +3,21 @@
 // Definitions by: Mohammed Hamdy <https://github.com/github-account-because-they-want-it>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+declare module "base64" {
+
+  /**
+   * Creates a base-64 encoded ASCII string from a string of binary data
+   * @param data the data to encode
+   * @param charset The charset of the string to encode (optional). The only accepted value is "utf-8".
+   *        In order to encode and decode Unicode strings, the charset parameter needs to be set
+   */
+  export function encode(data: string, charset?: string): string;
+
+  /**
+   *
+   * @param data the encoded data
+   * @param charset
+   */
+  export function decode(data: string, charset?: string): string;
+}
+
